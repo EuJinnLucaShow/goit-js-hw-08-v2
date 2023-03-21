fd32de121136faba6482ec829bd1b279a199aded
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-03-21 17:06:02 +0200

    Task 2 - video player
    Task 2 - video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
