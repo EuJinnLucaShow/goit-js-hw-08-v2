c083407d2293989cfe8fa9c75071f5d1b4d4e8a1
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-03-21 10:12:15 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+gallery.insertAdjacentHTML(
+  'afterbegin',
+  galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`
+    )
+    .join('')
+);
+
+new SimpleLightbox('.gallery a', {
+  overlayOpacity: 0.5,
+  captionDelay: 250,
+  captionsData: 'alt',
+  fadeSpeed: 250,
+  rtl: true,
+});
