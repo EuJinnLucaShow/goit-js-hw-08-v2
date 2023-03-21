53a26bd731d1e9c072da3e0e038baa5ea1addabb
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-03-21 21:44:58 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+// Отримати елементи форми
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const form = document.querySelector('.feedback-form');
 
+// Приєднатися до події введення на поля форми
+form.addEventListener('input', () => {
+  // Створити об'єкт стану форми
+  const feedbackState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  // Зберігати стан форми в локальному сховищі
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackState));
+});
+
+// Перевірити, чи є збережений стан форми в локальному сховищі
+const savedState = localStorage.getItem('feedback-form-state');
+
+if (savedState) {
+  // Заповнити поля форми збереженим станом
+  const parsedState = JSON.parse(savedState);
+  emailInput.value = parsedState.email;
+  messageInput.value = parsedState.message;
+}
+
+// Приєднатися до події надсилання форми
+form.addEventListener('submit', event => {
+  // Заборонити стандартну дію відправки форми
+  event.preventDefault();
+
+  if (emailInput.value === '' || messageInput.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  // Очистити локальне сховище
+  localStorage.removeItem('feedback-form-state');
+
+  // Вивести об'єкт з полями email та message та їхніми значеннями у консоль
+  const feedbackData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(feedbackData);
+
+  // Очистити поля форми
+  emailInput.value = '';
+  messageInput.value = '';
+});
